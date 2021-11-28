1e298a91164b5db4a3535e060e878d28bf2ede73
Aaron8907 <85762827+Aaron8907@users.noreply.github.com>
2021-11-28 17:31:57 -0600

    Test

--- public/index.js
+++ public/index.js
@@ -15,7 +15,7 @@ fetch("/api/transaction")
   });
 
 function populateTotal() {
-  console.log("test)")
+  console.log(")")
   // reduce transaction amounts to a single total value
   let total = transactions.reduce((total, t) => {
     return total + parseInt(t.value);
